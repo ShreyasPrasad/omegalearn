c763eeca389b0f1bdc82fc169ec79645aeee247f
chan-w <40780153+chan-w@users.noreply.github.com>
2021-01-16 00:40:18 -0500

    Add Flask SocketIO

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template, request, url_for, redirect
+from flask_socketio import SocketIO
 import os
 
 app = Flask(__name__)
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
+socketio = SocketIO(app)
+socketio.init_app(app, cors_allowed_origins="*")
 
 @app.route('/', methods=["GET", "POST"])
 def landing():
     return "Start Call"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='localhost')
+    socketio.run(app, debug=True, host='localhost')
 
                          
